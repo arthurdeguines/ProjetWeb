1127576d33339beee4b7fc2b2ff59dac3c9fa3b1
Arthur Deguines <deguinesarthur@gmail.com>
2017-11-20 18:17:41 +0100

    On peut maintenant switch entre connection et inscription

---
+++ js/connectionenregistrer.js
@@ -0,0 +1,13 @@
+$(function() {
+	$('#inscription').hide();
+    $('#connectionbtn').click(function(){
+    	console.log("coucou");
+        $('#connection').show();
+        $('#inscription').hide();
+    });
+    $('#enregistrerbtn').click(function(){
+        $('#inscription').show();
+         $('#connection').hide();
+
+    });
+});
