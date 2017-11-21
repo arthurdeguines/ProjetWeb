fae6aca705a613135cfd3ac79283206b175d3cb5
julien BOIREAU <julien.boireau@outlook.fr>
2017-11-21 11:46:18 +0100

    modification onglets

--- js/onglets.js
+++ js/onglets.js
@@ -2,13 +2,38 @@ $(function() {
 	$('#creer').hide();
     $('#categorie').hide();
     $('#contact').hide();
+
     $('#creerbtn').click(function(){
+
+        $('#categoriebtn').removeClass();
+        $('#creerbtn').removeClass();
+        $('#recettebtn').removeClass();
+        $('#contactbtn').removeClass();
+
+        $('#categoriebtn').addClass('col-2 btn btn-light');
+        $('#creerbtn').addClass('col-6 btn btn-warning');
+        $('#recettebtn').addClass('col-2 btn btn-light');
+        $('#contactbtn').addClass('col-2 btn btn-light');
+
         $('#creer').show();
         $('#categorie').hide();
         $('#contact').hide();
         $('#recette').hide();
     });
     $('#categoriebtn').click(function(){
+
+        $('#categoriebtn').removeClass();
+        $('#creerbtn').removeClass();
+        $('#recettebtn').removeClass();
+        $('#contactbtn').removeClass();
+
+        $('#categoriebtn').addClass('col-6 btn btn-warning');
+        $('#creerbtn').addClass('col-2 btn btn-light');
+        $('#recettebtn').addClass('col-2 btn btn-light');
+        $('#contactbtn').addClass('col-2 btn btn-light');
+
+
+
         $('#creer').hide();
         $('#categorie').show();
         $('#contact').hide();
@@ -16,6 +41,17 @@ $(function() {
 
     });
     $('#contactbtn').click(function(){
+
+        $('#categoriebtn').removeClass();
+        $('#creerbtn').removeClass();
+        $('#recettebtn').removeClass();
+        $('#contactbtn').removeClass();
+
+        $('#categoriebtn').addClass('col-2 btn btn-light');
+        $('#creerbtn').addClass('col-2 btn btn-light');
+        $('#recettebtn').addClass('col-2 btn btn-light');
+        $('#contactbtn').addClass('col-6 btn btn-warning');
+
         $('#creer').hide();
         $('#categorie').hide();
         $('#contact').show();
@@ -23,6 +59,17 @@ $(function() {
 
     });
     $('#recettebtn').click(function(){
+
+        $('#categoriebtn').removeClass();
+        $('#creerbtn').removeClass();
+        $('#recettebtn').removeClass();
+        $('#contactbtn').removeClass();
+
+        $('#categoriebtn').addClass('col-2 btn btn-light');
+        $('#creerbtn').addClass('col-2 btn btn-light');
+        $('#recettebtn').addClass('col-6 btn btn-warning');
+        $('#contactbtn').addClass('col-2 btn btn-light');
+
         $('#creer').hide();
         $('#categorie').hide();
         $('#contact').hide();
